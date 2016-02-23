1b29e1d82d94121389078e671170ac685dea0263
ivy <cengjingnameihao@sina.com>
2016-02-23 16:31:04 +0800

    LpHairCare

--- LPHairCare/js/basic.js
+++ LPHairCare/js/basic.js
@@ -4,21 +4,27 @@ $(function(){
 
     /*$('.box_inner--button').css("height",h+"px");*/
 
-    var select=w*0.3253;
+    var select=w*0.30625;
     $(".select").css("height",select+"px");
 
-    var select=w*1.01867;
+    var select=w*1.028125;
     $(".select").css("top",select+"px");
 
-    var little=w*0.108;
+    var little=w*0.10625;
     $(".little").css("height",little+"px");
     $(".middle").css("height",little+"px");
 
-    var alittle=w*0.0125;
+    var alittle=w*0.009375;
     $(".little").css("top",alittle+"px");
 
     var middle=w*0.2;
     $(".middle").css("top",middle+"px");
+
+    /*var alittle=w*1.034375;
+    $(".little").css("top",alittle+"px");
+
+    var middle=w*1.221875;
+    $(".middle").css("top",middle+"px");*/
 
 })
 
